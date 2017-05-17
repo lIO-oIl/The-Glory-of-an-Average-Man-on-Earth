6cf4a53d00aa720fae307cad6bf797f96019c8bd
lIO-oIl <arqanegaming@gmail.com>
2017-05-16 21:13:07 -0700

    menu done

--- G.A.M.E/gameRun.java
+++ G.A.M.E/gameRun.java
@@ -24,13 +24,19 @@ public class gameRun extends Application {
 
         Pane layout = new Pane();
 		playButton.relocate(125, 400);
+		playButton.setPrefHeight(36);
+		playButton.setPrefWidth(400);
 		instructionsButton.relocate(125, 436);
+		playButton.setPrefHeight(36);
+		instructionsButton.setPrefWidth(400);
 		settingsButton.relocate(125, 472);
+		playButton.setPrefHeight(36);
+		settingsButton.setPrefWidth(400);
 		creditsButton.relocate(125, 508);
-        layout.getChildren().add(playButton);
-        layout.getChildren().add(instructionsButton);
-        layout.getChildren().add(settingsButton);
-        layout.getChildren().add(creditsButton);
+		playButton.setPrefHeight(36);
+		creditsButton.setPrefWidth(400);
+        layout.getChildren().addAll(playButton, instructionsButton, settingsButton, creditsButton);
+		
         Scene game = new Scene(layout, 1280, 720);
 
         stage.setScene(game);
