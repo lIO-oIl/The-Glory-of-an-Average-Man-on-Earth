9fb280a82d647d5564173ffa6bbb9cee28e8cceb
lIO-oIl <arqanegaming@gmail.com>
2017-06-05 14:34:45 -0700

    .

--- src/Zone.java
+++ src/Zone.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zone
 {
-    public String mob[] = {"Hi"};
+    public List<Mob> mob = allMobs.getGameMobs();
     public String current = "action"; //action, combat
+    int rand = (int)Math.random() * 99 + 1;
+    int zoneCompletion = 0;
 
-    public void runZone(int input)
+    public void runZone(Location zone, int input)
     {
         if(current.equals("action")){
             if(input == 1)
@@ -12,8 +17,29 @@ public class Zone
                 scout();
         }
         if(current.equals("combat")){
-            if(input == 1)
-                mon set health = mob health - player weapon damage;
+            if(input == 1){}
         }
+    }
+
+    public void scout() {
+        if (rand <= 10)
+        {
+            findMob();
+        }
+    }
+
+    public void search() {
+        if (rand <= 70)
+        {
+            findMob();
+        }
+        else
+            zoneCompletion++;
+    }
+
+    public void findMob() {
+        current = "combat";
+        Mob randMob = mob.get((int)Math.random() * mob.size());
+
     }
 }
